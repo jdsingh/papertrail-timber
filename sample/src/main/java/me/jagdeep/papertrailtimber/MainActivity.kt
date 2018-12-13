daf48979704807f194a60a7110f63d3766f1f4a8
Jagdeep Singh <jagdeepsmundi@gmail.com>
2018-12-13 18:22:18 +0530

    refactor: Add buttons to log i,d,w,e logs in Sample app

--- sample/src/main/java/me/jagdeep/papertrailtimber/MainActivity.kt
+++ sample/src/main/java/me/jagdeep/papertrailtimber/MainActivity.kt
@@ -2,42 +2,42 @@ package me.jagdeep.papertrailtimber
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Timber.d("onCreate - called")
+        listOf<Button>(
+            findViewById(R.id.info),
+            findViewById(R.id.debug),
+            findViewById(R.id.warning),
+            findViewById(R.id.error)
+        ).forEach { btn ->
+            btn.setOnClickListener(this)
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Timber.i("onStart - called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Timber.i("onStop - called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Timber.i("onResume - called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Timber.i("onPause - called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        val ex = Exception("Something really bad")
-        Timber.e(ex, "onDestroy - called")
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.info -> {
+                Timber.i("Info")
+            }
+            R.id.debug -> {
+                Timber.d("Debug")
+            }
+            R.id.warning -> {
+                Timber.w("Warning")
+            }
+            R.id.error -> {
+                val ex = Exception("Something really bad")
+                Timber.e(ex, "Error")
+            }
+        }
     }
 
 }
