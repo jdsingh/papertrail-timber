33df69edee98b6d614fa93db7b337308ecd927a1
Jagdeep Singh <jagdeep.singh@go-jek.com>
2019-11-14 16:09:32 +0530

    chore: using Versions consts for all library versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,6 +5,11 @@ object Version {
 
     const val junit = "4.12"
     const val appCompact = "1.1.0"
+    const val timber = "4.7.1"
+    const val slf4j = "1.7.29"
+    const val logbackAndroidCore = "1.1.1-6"
+    const val logbackAndroidClassic = "1.1.1-6"
+    const val logbackSyslog4j = "1.0.0"
 }
 
 object BuildScript {
@@ -22,9 +27,9 @@ object Libraries {
     const val junit = "junit:junit:${Version.junit}"
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Version.kotlin}"
     const val appCompact = "androidx.appcompat:appcompat:${Version.appCompact}"
-    const val timber = "com.jakewharton.timber:timber:4.7.1"
-    const val slf4j = "org.slf4j:slf4j-api:1.7.29"
-    const val logbackAndroidCore = "com.github.tony19:logback-android-core:1.1.1-6"
-    const val logbackAndroidClassic = "com.github.tony19:logback-android-classic:1.1.1-6"
-    const val logbackSyslog4j = "com.papertrailapp:logback-syslog4j:1.0.0"
+    const val timber = "com.jakewharton.timber:timber:${Version.timber}"
+    const val slf4j = "org.slf4j:slf4j-api:${Version.slf4j}"
+    const val logbackAndroidCore = "com.github.tony19:logback-android-core:${Version.logbackAndroidCore}"
+    const val logbackAndroidClassic = "com.github.tony19:logback-android-classic:${Version.logbackAndroidClassic}"
+    const val logbackSyslog4j = "com.papertrailapp:logback-syslog4j:${Version.logbackSyslog4j}"
 }
